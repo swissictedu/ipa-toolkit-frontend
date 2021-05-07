b4afebe284d8d892230c481931c8ae93fbc03183
Robin <1105080+openscript@users.noreply.github.com>
2021-05-07 08:31:49 +0000

    Initialize app

---
+++ src/configuration.ts
@@ -0,0 +1,7 @@
+const CONFIGURATION = {
+  env: {
+    api: 'http://localhost:3000/graphql'
+  }
+};
+
+export default CONFIGURATION;
---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import CONFIGURATION from './configuration';
+
+const client = new ApolloClient({ uri: CONFIGURATION.env.api, cache: new InMemoryCache() });
+
+function AppShell() {
+  return (
+    <React.StrictMode>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.render(<AppShell />, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
