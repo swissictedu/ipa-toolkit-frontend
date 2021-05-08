710a0d831367d3d2a24386797a0607c0d9f08fa8
Robin <1105080+openscript@users.noreply.github.com>
2021-05-08 09:55:12 +0000

    Configure graphql codegen

---
+++ graphql-types.ts
@@ -0,0 +1,32 @@
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  /** An example field added by the generator */
+  testField: Scalars['String'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  /** An example field added by the generator */
+  testField: Scalars['String'];
+};
+
+export type TestFieldQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type TestFieldQuery = (
+  { __typename?: 'Query' }
+  & Pick<Query, 'testField'>
+);
