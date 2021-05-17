2dc6b677a0fc268e8b83445ac817eb14d7540a46
Robin <1105080+openscript@users.noreply.github.com>
2021-05-17 10:16:27 +0000

    Enhance connection form

--- src/components/conference/Connection.tsx
+++ src/components/conference/Connection.tsx
@@ -12,14 +12,19 @@ export default function Connection({ loading = false, checkConnection }: Connect
 
   return (
     <Form<Credentials> layout="vertical" onFinish={checkConnection}>
-      <Form.Item label={intl.formatMessage({ id: 'label.pkorg-endpoint' })} name="baseUrl" rules={[{ required: true }]}>
+      <Form.Item
+        label={intl.formatMessage({ id: 'label.pkorg-endpoint' })}
+        name="baseUrl"
+        rules={[{ required: true }]}
+        initialValue={intl.formatMessage({ id: 'label.pkorg-endpoint-default' }, { year: new Date().getFullYear() })}
+      >
         <Input placeholder={intl.formatMessage({ id: 'label.pkorg-endpoint-placeholder' }, { year: new Date().getFullYear() })} disabled={loading} />
-      </Form.Item>
-      <Form.Item label={intl.formatMessage({ id: 'label.session-token' })} name="sessionToken" rules={[{ required: true }]}>
-        <Input placeholder={intl.formatMessage({ id: 'label.session-token-placeholder' })} disabled={loading} />
       </Form.Item>
       <Form.Item label={intl.formatMessage({ id: 'label.user-agent' })} name="userAgent" rules={[{ required: true }]} initialValue={navigator.userAgent}>
         <Input disabled={loading} />
+      </Form.Item>
+      <Form.Item label={intl.formatMessage({ id: 'label.session-token' })} name="sessionToken" rules={[{ required: true }]}>
+        <Input placeholder={intl.formatMessage({ id: 'label.session-token-placeholder' })} disabled={loading} />
       </Form.Item>
       <Form.Item>
         <Button type="primary" htmlType="submit" loading={loading}>
