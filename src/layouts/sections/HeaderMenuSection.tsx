564b3e3c7c1a84e63617bf1549691e72be78fce6
Robin <1105080+openscript@users.noreply.github.com>
2021-05-25 09:43:38 +0000

    Fix collapsing header menu

--- src/layouts/sections/HeaderMenuSection.tsx
+++ src/layouts/sections/HeaderMenuSection.tsx
@@ -1,12 +1,20 @@
 import { LogoutOutlined } from '@ant-design/icons';
+import { css } from '@emotion/react';
 import { Menu } from 'antd';
 import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
 import CONFIGURATION from '../../configuration';
 
+// TODO: Remove after fixed in ant
+const menuWidth = css`
+  min-width: 15rem;
+  display: flex;
+  justify-content: flex-end;
+`;
+
 export default function HeaderMenuSection() {
   return (
-    <Menu theme="dark" mode="horizontal" selectable={false}>
+    <Menu theme="dark" mode="horizontal" selectable={false} css={menuWidth}>
       <Menu.Item key="sign-out" icon={<LogoutOutlined />}>
         <Link to={CONFIGURATION.paths.signOut}>
           <FormattedMessage id="label.sign-out" />
