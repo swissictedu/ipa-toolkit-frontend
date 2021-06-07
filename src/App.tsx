ff234e815730548a07bcfd605d39270d799b7c3d
Robin <1105080+openscript@users.noreply.github.com>
2021-06-07 11:48:55 +0000

    Add minimal error boundry

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import { IntlProvider } from 'react-intl';
 import Router from './Router';
 import { getMessages } from './utils/translations';
 import deDE from 'antd/lib/locale/de_DE';
+import ErrorBoundary from './views/ErrorBoundary';
 
 function App() {
   const locale = 'de-ch';
 
   return (
-    <IntlProvider locale={locale} messages={getMessages(locale)}>
-      <ConfigProvider locale={deDE}>
-        <Router />
-      </ConfigProvider>
-    </IntlProvider>
+    <ErrorBoundary>
+      <IntlProvider locale={locale} messages={getMessages(locale)}>
+        <ConfigProvider locale={deDE}>
+          <Router />
+        </ConfigProvider>
+      </IntlProvider>
+    </ErrorBoundary>
   );
 }
 
